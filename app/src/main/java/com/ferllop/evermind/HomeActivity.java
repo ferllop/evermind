39f4a36eb22668d6b2840cba3378873c1c9f9063
Ferran <ferranllop@gmail.com>
2021-03-03 09:44:12 +0100

    Link search button to search activity

--- app/src/main/java/com/ferllop/evermind/HomeActivity.java
+++ app/src/main/java/com/ferllop/evermind/HomeActivity.java
@@ -2,7 +2,10 @@ package com.ferllop.evermind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button button = findViewById(R.id.search_cards_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, SearchCardsActivity.class));
+            }
+        });
     }
 }
