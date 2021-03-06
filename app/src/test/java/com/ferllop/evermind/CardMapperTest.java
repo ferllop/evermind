2b6d2e882c068b2ef466d60a520009de9389b7f2
Ferran <ferranllop@gmail.com>
2021-03-06 18:50:34 +0100

    Create tests for CardMapper

--- app/src/test/java/com/ferllop/evermind/CardMapperTest.java
+++ app/src/test/java/com/ferllop/evermind/CardMapperTest.java
@@ -1,4 +1,112 @@
 package com.ferllop.evermind;
 
+import com.ferllop.evermind.models.Card;
+import com.ferllop.evermind.models.Labelling;
+import com.ferllop.evermind.repositories.mappers.CardMapper;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class CardMapperTest {
+
+    String id;
+    String author;
+    String question;
+    String answer;
+    String label1;
+    String label2;
+    Map<String, Object> cardAsMap;
+
+    Card cardAsCard;
+
+    @Before
+    public void setId(){
+        id = "xyz";
+        author = "TheAuthor";
+        question = "The question?";
+        answer = "The answer";
+        label1 = "label1";
+        label2 = "label2";
+    }
+
+    public void createCardAsCard(){
+        cardAsCard = new Card(id, author, question, answer, new Labelling(Arrays.asList(label1, label2)));
+    }
+
+
+    public void createCardAsMap(){
+         Map<String, Boolean> labelling = new HashMap<>();
+        labelling.put(label1, true);
+        labelling.put(label2, true);
+
+        cardAsMap = new HashMap<>();
+        cardAsMap.put("author", author);
+        cardAsMap.put("question", question);
+        cardAsMap.put("answer", answer);
+        cardAsMap.put("labels", labelling);
+    }
+
+    @Test
+    public void should_convert_a_Card_to_a_String_key_Object_value_Map_with_an_author(){
+        createCardAsCard();
+        assertEquals(author, new CardMapper().execute(cardAsCard).get("author"));
+    }
+
+    @Test
+    public void should_convert_a_Card_to_a_String_key_Object_value_Map_with_a_question(){
+        createCardAsCard();
+        assertEquals(question, new CardMapper().execute(cardAsCard).get("question"));
+    }
+
+    @Test
+    public void should_convert_a_Card_to_a_String_key_Object_value_Map_with_an_answer(){
+        createCardAsCard();
+        assertEquals(answer, new CardMapper().execute(cardAsCard).get("answer"));
+    }
+
+    @Test
+    public void should_convert_a_Card_to_a_String_key_Object_value_Map_with_labels(){
+        createCardAsCard();
+        Map<String, Boolean> labelling = (Map<String, Boolean>) new CardMapper().execute(cardAsCard).get("labels");
+
+        assertTrue(labelling.get(label1));
+        assertTrue(labelling.get(label2));
+    }
+
+    @Test
+    public void should_convert_a_String_key_Object_value_map_to_a_Card_with_an_id(){
+        createCardAsMap();
+        assertEquals(id, new CardMapper().execute(id, cardAsMap).getId());
+
+    }@Test
+    public void should_convert_a_String_key_Object_value_map_to_a_Card_with_an_author(){
+        createCardAsMap();
+        assertEquals("TheAuthor", new CardMapper().execute(id, cardAsMap).getAuthor());
+    }
+
+    @Test
+    public void should_convert_a_String_key_Object_value_map_to_a_Card_with_a_question(){
+        createCardAsMap();
+        assertEquals("The question?", new CardMapper().execute(id, cardAsMap).getQuestion());
+    }
+
+    @Test
+    public void should_convert_a_String_key_Object_value_map_to_a_Card_with_an_answer(){
+        createCardAsMap();
+        assertEquals("The answer", new CardMapper().execute(id, cardAsMap).getAnswer());
+    }
+
+    @Test
+    public void should_convert_a_String_key_Object_value_map_to_a_Card_with_labels(){
+        createCardAsMap();
+        assertEquals("label1", new CardMapper().execute(id, cardAsMap).getLabelling().getLabels().get(0));
+        assertEquals("label2", new CardMapper().execute(id, cardAsMap).getLabelling().getLabels().get(1));
+    }
 }
